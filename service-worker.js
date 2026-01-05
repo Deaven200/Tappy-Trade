f0bfb272ea8d03cedd665fe56ca336318d1141d7
Deaven200 <deaven200@gmail.com>
2026-01-05 11:53:31 -0600

    1
    1

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // Tappy Trade Service Worker
 // Handles caching and automatic updates
 
-const CACHE_VERSION = 'tappy-trade-v1.0.4';
+const CACHE_VERSION = 'tappy-trade-v1.0.5';
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
